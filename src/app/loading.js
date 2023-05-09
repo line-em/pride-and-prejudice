b31b8c35fc9f6aa683ee3a81c5d00e1b6442a2e5
Line-em <alineemily@hotmail.com>
2023-05-09 11:19:36 +0100

    remove persist

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,13 +1,13 @@
-import React from "react";
+"use client";
 import { ProgressSpinner } from "primereact/progressspinner";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-dark-purple/theme.css";
 
 function loading() {
 	return (
-		<div>
+		<>
 			<ProgressSpinner strokeWidth="5" />
-		</div>
+		</>
 	);
 }
 
--- src/store/useLayoutStore.jsx
+++ src/store/useLayoutStore.jsx
@@ -1,71 +1,56 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
 import { rubik, literata, montserrat, mono, lora } from "@/app/layout";
 
-const useLayoutStore = create(
-	persist(
-		(set) => ({
-			fontSize: "1rem",
-			fonts: [
-				rubik.className,
-				lora.className,
-				montserrat.className,
-				literata.className,
-				mono.className
-			],
-			textColor: "#333",
-			textAccentColor: "#4caf50",
-			backgroundColorAlt: "#f4f4f4",
-			backgroundColor: "#fff",
+const useLayoutStore = create((set) => ({
+	fontSize: "1rem",
+	fonts: [
+		rubik.className,
+		lora.className,
+		montserrat.className,
+		literata.className,
+		mono.className
+	],
+	textColor: "#333",
+	textAccentColor: "#4caf50",
+	backgroundColorAlt: "#f4f4f4",
+	backgroundColor: "#fff",
 
-			setFont: (fontIndex) =>
-				set((state) => {
-					document.body.classList.remove(...state.fonts.map((f) => f));
-					document.body.classList.add(fontIndex);
-					return { fonts: state.fonts, fontSize: state.fontSize };
-				}),
-
-			setSize: (value) =>
-				set((state) => {
-					const storyElement = document.querySelector(".story");
-					if (storyElement) {
-						storyElement.style.setProperty("--font-size", value);
-						return { fontSize: value, fonts: state.fonts };
-					} else {
-						return { fonts: state.fonts, fontSize: state.fontSize };
-					}
-				}),
-
-			setTheme: (textColor, textAccentColor, backgroundColorAlt, backgroundColor) =>
-				set((state) => {
-					document.documentElement.style.setProperty("--text", textColor);
-					document.documentElement.style.setProperty(
-						"--text-accent",
-						textAccentColor
-					);
-					document.documentElement.style.setProperty(
-						"--background-alt",
-						backgroundColorAlt
-					);
-					document.documentElement.style.setProperty(
-						"--background",
-						backgroundColor
-					);
-					return {
-						textColor: textColor,
-						textAccentColor: textAccentColor,
-						backgroundColorAlt: backgroundColorAlt,
-						backgroundColor: backgroundColor,
-						fonts: state.fonts,
-						fontSize: state.fontSize
-					};
-				})
+	setFont: (fontIndex) =>
+		set((state) => {
+			document.body.classList.remove(...state.fonts.map((f) => f));
+			document.body.classList.add(fontIndex);
+			return { fonts: state.fonts, fontSize: state.fontSize };
 		}),
-		{
-			name: "textStyling",
-			getStorage: () => createJSONStorage(() => sessionStorage)
-		}
-	)
-);
+
+	setSize: (value) =>
+		set((state) => {
+			const storyElement = document.querySelector(".story");
+			if (storyElement) {
+				storyElement.style.setProperty("--font-size", value);
+				return { fontSize: value, fonts: state.fonts };
+			} else {
+				return { fonts: state.fonts, fontSize: state.fontSize };
+			}
+		}),
+
+	setTheme: (textColor, textAccentColor, backgroundColorAlt, backgroundColor) =>
+		set((state) => {
+			document.documentElement.style.setProperty("--text", textColor);
+			document.documentElement.style.setProperty("--text-accent", textAccentColor);
+			document.documentElement.style.setProperty(
+				"--background-alt",
+				backgroundColorAlt
+			);
+			document.documentElement.style.setProperty("--background", backgroundColor);
+			return {
+				textColor: textColor,
+				textAccentColor: textAccentColor,
+				backgroundColorAlt: backgroundColorAlt,
+				backgroundColor: backgroundColor,
+				fonts: state.fonts,
+				fontSize: state.fontSize
+			};
+		})
+}));
 
 export default useLayoutStore;
