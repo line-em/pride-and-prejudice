ebf7dd3e7d37b6355f1f0c8297e469dbed6c80e0
Line-em <alineemily@hotmail.com>
2023-11-02 00:28:41 +0000

    more info at footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -52,7 +52,24 @@ export default function RootLayout({ children }) {
 				</main>
 				<footer className="center">
 					<hr />
-					Book by Jane Austen. Coding by Line.
+					Book by Jane Austen. Coding by Line. Special thanks to
+					<Link
+						href="https://standardebooks.org/"
+						className="no-button flex"
+						style={{ fontSize: ".8rem" }}
+						target="_blank"
+					>
+						Standardebooks
+					</Link>{" "}
+					and{" "}
+					<Link
+						href="https://gutenberg.org/"
+						className="no-button flex"
+						style={{ fontSize: ".8rem" }}
+						target="_blank"
+					>
+						Project Gutenberg.
+					</Link>
 				</footer>
 			</body>
 		</html>
