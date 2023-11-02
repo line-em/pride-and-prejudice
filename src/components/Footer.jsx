8b01c2064b93e5fcd2aa92254190e6e0c1b701ae
Line-em <alineemily@hotmail.com>
2023-11-02 00:48:43 +0000

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<footer className="center">
+			<hr />
+			Book by Jane Austen. Coding by Line. Special thanks to
+			<Link
+				href="https://standardebooks.org/"
+				className="no-button flex"
+				style={{ fontSize: ".8rem" }}
+				target="_blank"
+			>
+				Standardebooks
+			</Link>{" "}
+			and{" "}
+			<Link
+				href="https://gutenberg.org/"
+				className="no-button flex"
+				style={{ fontSize: ".8rem" }}
+				target="_blank"
+			>
+				Project Gutenberg.
+			</Link>
+		</footer>
+	);
+};
+
+export default Footer;
